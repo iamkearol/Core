31f354a55f4ac432218269c6cb92d1f4ea2834a4
kearol lui <admin@c0re.moe>
2020-05-02 16:26:06 +0800

    update master

--- src/model/User.go
+++ src/model/User.go
@@ -1,8 +1,10 @@
 package model
-
+import time
 type User struct{
 	ID uint 
 	Username string
 	Password string
 	Email email
+	Dob time.Time
+	Gender string
 }
---
+++ src/model/UserDetail.go
@@ -0,0 +1,8 @@
+package model
+type UserDetail struct{
+	ID uint 
+	Userid int
+	Contry int
+	Address string
+	
+}
